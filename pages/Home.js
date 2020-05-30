ea9d4a93bc5884feabd2b2d244837fec991247a4
poppin <yannisse.abbad@epitech.eu>
2020-05-30 02:35:38 +0200

    29/05

--- pages/Home.js
+++ pages/Home.js
@@ -1,29 +1,103 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native'
+import { StyleSheet, Text, View, Image, Button, AsyncStorage } from 'react-native'
 
 export default class HomePage extends React.Component {
-  
+
+
+
+
   render() {
     const { navigate } = this.props.navigation;
-    return (
-      <View style={{
-        backgroundColor: 'yellow',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <View><Image source={{ uri: 'https://docs.snapchat.com/static/7619bf5537237fa6abac3ddcfc1d379b/55a47/ghostlogo%402x.png' }} style={{ width: 70, height: 70 }} /></View>
-        <View style={{ flexDirection: "row" }}>
-          <Button
-            title="Login"
-            onPress={() => navigate('LoginPage')}
-          />
-          <Button
-            title='Register'
-            onPress={() => navigate('RegisterPage')}
-          />
+    // displayData = async ()=>{  
+    // let user = await AsyncStorage.getItem('userData');
+
+    // let parsed = JSON.parse(user);
+
+
+
+    // }
+    
+    // if (parsed.token !== '' || parsed.token !== null) {
+    //     console.log('zebiiiii');
+
+    // } else {
+
+//       displayData = async ()=>{  
+//     try{  
+//       let user = await AsyncStorage.getItem('token');  
+//       // let parsed = JSON.parse(user);  
+//       // alert(user);
+//       if(user !== null){
+//       // alert(user);
+
+//         return true;
+//       }
+//     }  
+//     catch(error){  
+//       alert(error)  
+//     }  
+//   }  
+// if(displayData() == true){
+  
+//   return (
+    
+//     // navigate('RegisterPage')
+//     // <View>
+//     //   <Button title="TEST"></Button>
+//     // </View>
+    
+
+//     <View style={{
+//       backgroundColor: 'yellow',
+//       flex: 1,
+//       justifyContent: 'center',
+//       alignItems: 'center'
+//     }}>
+//       <View><Image source={{ uri: 'https://docs.snapchat.com/static/7619bf5537237fa6abac3ddcfc1d379b/55a47/ghostlogo%402x.png' }} style={{ width: 70, height: 70 }} /></View>
+//       <View style={{ flexDirection: "row" }}>
+//         <Button
+//           title="Login"
+//           onPress={() => navigate('LoginPage')}
+//         />
+//         <Button
+//           title='Register'
+//           onPress={() => navigate('RegisterPage')}
+//         />
+//       </View>
+
+//     </View>
+
+//   )
+// }else{
+
+ return (
+   
+      
+        <View style={{
+          backgroundColor: 'yellow',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          <View><Image source={{ uri: 'https://docs.snapchat.com/static/7619bf5537237fa6abac3ddcfc1d379b/55a47/ghostlogo%402x.png' }} style={{ width: 70, height: 70 }} /></View>
+          <View style={{ flexDirection: "row" }}>
+            <Button
+              title="Login"
+              onPress={() => navigate('LoginPage')}
+            />
+            <Button
+              title='Register'
+              onPress={() => navigate('RegisterPage')}
+            />
+          </View>
+
         </View>
-      </View>
-    )
-  }
+      )
+
 }
+     
+
+    }
+
+  // }
+// }
--- pages/Login.js
+++ pages/Login.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, Button, AsyncStorage } from 'react-native';
 import { useForm } from "react-hook-form";
 import { color } from 'react-native-reanimated';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import HomePage from "./Home";
 
 
 
-export default function LoginPage() {
+export default function LoginPage({ navigate }) {
 
   const { register, setValue, handleSubmit, errors } = useForm();
   // const onSubmit = data => Alert.alert("Form Data", JSON.stringify(data));
@@ -16,10 +17,10 @@ export default function LoginPage() {
   //   register({ name: "password" }, { required: true });
   // }, [register]);
 
-  const onSubmit = function (data) {
+  onSubmit = async (data) => {
     // console.log(data.email);
     // console.log(data.password);
-    console.log(data)
+    // console.log(data)
     // fetch('http://snapi.epitech.eu/inscription', {
     //   method: 'POST',
     //   headers: {
@@ -31,51 +32,132 @@ export default function LoginPage() {
     //     password: data.password
     //   })
     // });
-    fetch('http://snapi.epitech.eu/connection', {
+    const response = await fetch('http://snapi.epitech.eu/connection', {
       method: 'POST',
       body: JSON.stringify(data),
       headers: new Headers({
         'Content-Type': 'application/json'
       })
-    }).then(res => res.json())
-  .catch(error => console.error('Error: ', error))
-  .then(response => console.log('Success: ', response))
+    })/* .then(res => res.json())
+      .catch(error => console.error('Error: ', error))
+      .then(response => console.log('Success: ', response.data.token),
+      ) */
+      .then((response) => response.json())
+    if (response.data.token === undefined)
+      console.log('response.data.token');
+    else {
+      await AsyncStorage.setItem("token", response.data.token);
+      // this.setState({showHome: true});
+      // console.log(response.data.token);
+      let user = await AsyncStorage.getItem('token');
+      // let parsed = JSON.parse(user);  
+      // console.log('success :' + parsed.token); 
+      console.log('success :' + user);
+      return (
+        <View>
+          <Button title="TEST"></Button>
+        </View>
+      )
+
+
+    }
+    // .then((response) => {
+    //   console.log(response);
+    // //  AsyncStorage.setItem('userData', response.data.token)
+    // //  let user = AsyncStorage.getItem('userData');  
+    // //  console.log('success :' + user);  
+
+
+
+    //   // return navigate('HomePage')
+
+    // })
   }
   useEffect(() => {
     register({ name: "email" }, { required: true });
     register({ name: "password" }, { required: true });
   }, [register]);
 
-  return (
 
-    
-    <View style={{
-      backgroundColor: 'yellow',
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
+  // displayData = async () => {
+  //   try {
+  //     let user = await AsyncStorage.getItem('userData');
+  //     let parsed = JSON.parse(user);
+  //     console.log('success :' + parsed.token);
+
+  //     alert(parsed.token);
+  //   }
+  //   catch (error) {
+  //     alert(error)
+  //   }
+  // }
+  displayData = async () => {
+    var found = false;
+    // try {
+      let user =  await AsyncStorage.getItem('token');
+      // let parsed = JSON.parse(user);  
+      alert(user);
+      console.log(user);
+      if (user !== null) {
+        alert(user);
+
+        found = true
+      }
+    // }
+    // catch (error) {
+    //   alert(error)
+    // }
+    return  found
+  }
+
+  if (displayData() == true) {
+    alert(displayData())
+
+
+    return (
+      <View>
+        <Button title="TEST"></Button>
+      </View>
+    )
+
+  } else {
+    return (
+
+
       <View style={{
-        margin: 30
-      }}
-      ><Text style={{ color : 'white', fontSize:50, fontWeight:'bold'}}>Login</Text></View>
-      <Text>Email</Text>
-      <TextInput
-        style={{ height: 40, width : 200, borderColor: 'grey', borderWidth: 1, backgroundColor : 'white', borderRadius : 5}}
-        onChangeText={text => setValue("email", text, true)}
-      />
-      {errors.email && <Text>This is required.</Text>}
-      <Text>Password</Text>
-      <TextInput
-        style={{ height: 40, width : 200, borderColor: 'grey', borderWidth: 1, backgroundColor : 'white', borderRadius : 5 }}
-        secureTextEntry={true}
-        onChangeText={text => setValue("password", text, true)}
-      />
-      {errors.password && <Text>This is required.</Text>}
-      <Button title='Valider' onPress={handleSubmit(onSubmit)}/>
-     
-    </View>
-  );
+        backgroundColor: 'yellow',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+
+      }}>
+
+        <View style={{
+          margin: 30
+        }}
+
+        ><Text style={{ color: 'white', fontSize: 50, fontWeight: 'bold' }}>Login</Text></View>
+        <Text>Email</Text>
+        <TextInput
+          style={{ height: 40, width: 200, borderColor: 'grey', borderWidth: 1, backgroundColor: 'white', borderRadius: 5 }}
+          onChangeText={text => setValue("email", text, true)}
+        />
+        {errors.email && <Text>This is required.</Text>}
+        <Text>Password</Text>
+        <TextInput
+          style={{ height: 40, width: 200, borderColor: 'grey', borderWidth: 1, backgroundColor: 'white', borderRadius: 5 }}
+          secureTextEntry={true}
+          onChangeText={text => setValue("password", text, true)}
+        />
+        {errors.password && <Text>This is required.</Text>}
+        <Button title='Valider' onPress={handleSubmit(onSubmit)} />
+
+      </View>
+    )
+
+
+  }
+
 
 
 }
